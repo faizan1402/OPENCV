556152f66d56f4ce2d77432edc0ac7edb49a3e32
Faizan Ahmad <officialfaizanahmad@gmail.com>
2020-02-28 23:26:42 +0530

    rotation

---
+++ Rotation.py
@@ -0,0 +1,21 @@
+import cv2
+import numpy as np
+img =cv2.imread('lena.jpg')
+height ,width =img.shape[:2]
+rotation_matrix = cv2.getRotationMatrix2D((width/2,height/2),70,.5)
+
+#getRotationMatrix2D is a function and rotate the img and (width,height ,angle,scaling factor)
+#Means scaling factor will increase then img information destroy and scaling factor is decrease then img of information increase
+
+
+#This rotation_matrix through image rotate
+#Syntax of cv2: rotate image
+# M(Matrix) = cv2.getRotationMatrix2D(center, angle, scale)
+  #rotated = cv2.warpAffine(img, M, (w, h))
+rotated_img = cv2.warpAffine(img , rotation_matrix , (width,height))
+cv2.imshow("Rotated Image", rotated_img)
+cv2.imshow("Original Image",img)
+
+
+cv2.waitKey(0)
+cv2.destroyAllWindows()
