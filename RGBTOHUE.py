3a5dab51e58210638fc3350de2b3b3db88eac23f
Faizan Ahmad <officialfaizanahmad@gmail.com>
2020-02-28 23:25:20 +0530

    HUE

---
+++ RGBTOHUE.py
@@ -0,0 +1,5 @@
+#How to convert RGB to HSV color (H=Hue means all color show the image)
+# (S=Saturation saturation means any particular color)
+# (V=Value means intensity low to high intensity)
+#Color filter and color space
+#HUe =0 to 180 ,Saturation = 0 to 255 ,Value = 0 to 255
