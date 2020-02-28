25fb3bd4eefc76c8640ff3d6b50a1998629556c2
Faizan Ahmad <officialfaizanahmad@gmail.com>
2020-02-28 22:59:43 +0530

    hsv

---
+++ faizanhsv.py
@@ -0,0 +1,21 @@
+import cv2
+
+img= cv2.imread("faizan.jpg")
+print("Original Dimensions :",img.shape)
+
+img_HSV =cv2.cvtColor(img,cv2.COLOR_BGR2HSV)
+cv2.imshow("HSV Image",img_HSV)
+ 
+cv2.imshow('Hue channel',img_HSV[:,:,0])
+cv2.imshow('Saturation',img_HSV[:,:,1])
+cv2.imshow('Value Channel',img_HSV[:,:,2])
+
+cv2.waitKey(0)
+
+cv2.destroyAllWindows()
+
+
+
+#Out put
+
+#Original Dimensions : (3840, 1958, 3) (height,width,channel)
